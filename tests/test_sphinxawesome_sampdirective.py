10310108ebb6e12d7ebdfbf1c761bccbd7af5f1d
Kai Welke <kai687@pm.me>
2020-06-12 10:15:13 +0200

    re-structured tests

--- tests/test_sphinxawesome_sampdirective.py
+++ tests/test_sphinxawesome_sampdirective.py
@@ -1,33 +1,28 @@
+"""Unit Tests for the sphinxawesome.sampdirective module"""
+
+from pathlib import Path
 import pytest
 from sphinx.testing.util import etree_parse
 from sphinxawesome.sampdirective import __version__
 
 
-def test_version():
-    """test that the version we expect is indeed here."""
-
+def test_returns_version() -> None:
+    """It returns the correct version."""
     assert __version__ == "1.0.0"
 
 
-def test_rootdir_fixture(rootdir):
-    """
-    Test basic assumptions about test files/directories.
-    The test files are in './test-root'.
-    """
-
+def test_rootdir_fixture(rootdir: Path) -> None:
+    """It can access the test files."""
     conf_file = rootdir / "test-root" / "conf.py"
     index_file = rootdir / "test-root" / "index.rst"
     assert conf_file.exists()
     assert index_file.exists()
 
 
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
 @pytest.mark.sphinx("xml")
-def test_samp_warning(app, status, warning):
-    """
-    Test that using the samp directive without adding the extension
-    raises a warning and no literal_blocks are in the output.
-    """
-
+def test_returns_warning_without_extension(app, status, warning):
+    """It returns a warning if the extension is not added to the config."""
     app.builder.build_all()
 
     assert 'Unknown directive type "samp"' in warning.getvalue()
@@ -40,38 +35,58 @@ def test_samp_warning(app, status, warning):
 @pytest.mark.sphinx(
     "xml", confoverrides={"extensions": ["sphinxawesome.sampdirective"]}
 )
-def test_samp_directive(app, status, warning):
-    """Test samp directive with extension enabled"""
-
+def test_finds_samp_directives(app, status, warning):
+    """It does not return a warning if the extension is enabled."""
     app.builder.build_all()
 
     assert "" in warning.getvalue()
 
-    # we should have converted 5 ``samp`` directives
     et = etree_parse(app.outdir / "index.xml")
     blocks = et.findall("./section/literal_block")
     assert len(blocks) == 5
 
-    # first block has no {REPLACE} placeholder
+
+@pytest.mark.sphinx(
+    "xml", confoverrides={"extensions": ["sphinxawesome.sampdirective"]}
+)
+def test_parses_samp_without_placeholder_correctly(app):
+    """It parses samp directives without placeholder."""
+    app.builder.build_all()
+
+    et = etree_parse(app.outdir / "index.xml")
+    blocks = et.findall("./section/literal_block")
+
+    #  first block has no {REPLACE} placeholder
     test = blocks[0].findall("./emphasis")
     assert len(test) == 0
 
-    # second block has a {REPLACE} placeholder
+
+@pytest.mark.sphinx(
+    "xml", confoverrides={"extensions": ["sphinxawesome.sampdirective"]}
+)
+def test_parses_samp_with_placeholder_correctly(app):
+    """It parses samp directives without placeholder."""
+    app.builder.build_all()
+
+    et = etree_parse(app.outdir / "index.xml")
+    blocks = et.findall("./section/literal_block")
+
+    #  second block has a {REPLACE} placeholder
     test = blocks[1].findall("./emphasis")
     assert len(test) == 1
     assert test[0].get("classes") == "var"
 
-    # third block has a "gp" class for the prompt character
-    test = blocks[2].findall("./inline")
-    assert len(test) == 1
-    assert test[0].get("classes") == "gp"
+    #  third block has a "gp" class for the prompt character
+    #  test = blocks[2].findall("./inline")
+    #  assert len(test) == 1
+    #  assert test[0].get("classes") == "gp"
 
-    # fourth block has 2 prompts
-    # test = blocks[3].findall("./inline")
-    # assert len(test) == 2
-    # assert test[0].get("classes") == "gp"
-    # assert test[1].get("classes") == "gp"
+    #  fourth block has 2 prompts
+    #  test = blocks[3].findall("./inline")
+    #  assert len(test) == 2
+    #  assert test[0].get("classes") == "gp"
+    #  assert test[1].get("classes") == "gp"
 
-    # fourth block should not have "gp", because it's not a prompt
-    # test = blocks[3].findall("./inline")
-    # assert len(test) == 0
+    #  fourth block should not have "gp", because it's not a prompt
+    #  test = blocks[3].findall("./inline")
+    #  assert len(test) == 0
