03f15f6c0f5a18b0b3cdc4d87b0bcd3fb836f7e0
Kai Welke <kai687@pm.me>
2020-10-19 10:20:47 +0200

    chore: version bump

--- tests/test_sphinxawesome_sampdirective.py
+++ tests/test_sphinxawesome_sampdirective.py
@@ -12,7 +12,7 @@ from sphinxawesome.sampdirective import __version__
 
 def test_returns_version() -> None:
     """It returns the correct version."""
-    assert __version__ == "1.0.4"
+    assert __version__ == "1.0.5"
 
 
 def test_rootdir_fixture(rootdir: Path) -> None:
