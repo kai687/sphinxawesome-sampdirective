a70e2399f707c2d5df6b0106363233687535afee
Kai Welke <kai.welke@algolia.com>
2022-03-07 10:26:52 +0100

    chore: nox

--- noxfile.py
+++ noxfile.py
@@ -1,49 +1,31 @@
 """Nox sessions."""
 
 import tempfile
-from typing import Any
 
 import nox
-from nox.sessions import Session
+from nox_poetry import Session, session
 
 nox.options.stop_on_first_error = True
 nox.options.sessions = ["tests", "lint", "mypy", "safety"]
 locations = ["src", "tests", "noxfile.py"]
-python_versions = ["3.6", "3.7", "3.8", "3.9"]
+python_versions = ["3.7", "3.8", "3.9", "3.10"]
 
 
-def install_constrained_version(session: Session, *args: str, **kwargs: Any) -> None:
-    """Install packages with version constraints from poetry.lock."""
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--format=requirements.txt",
-            "--without-hashes",
-            "--dev",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
-
-
-@nox.session(python=python_versions)
-@nox.parametrize("sphinx", ["3.*", "4.*"])
-def tests(session: Session, sphinx: str) -> None:
+@session(python=python_versions)
+def tests(session: Session) -> None:
     """Run unit tests."""
     args = session.posargs or ["--cov"]
+    deps = ["coverage[toml]", "pytest", "pytest-cov"]
     session.run("poetry", "install", "--no-dev", external=True)
-    install_constrained_version(session, "coverage[toml]", "pytest", "pytest-cov")
-    session.install(f"sphinx=={sphinx}")
+    session.install(".", *deps)
     session.run("pytest", *args)
 
 
-@nox.session(python=python_versions)
+@session(python=python_versions)
 def lint(session: Session) -> None:
     """Lint with Flake8."""
     args = session.posargs or locations
-    install_constrained_version(
-        session,
+    deps = [
         "flake8",
         "flake8-annotations",
         "flake8-bandit",
@@ -51,30 +33,30 @@ def lint(session: Session) -> None:
         "flake8-bugbear",
         "flake8-docstrings",
         "flake8-implicit-str-concat",
-    )
+    ]
+    session.install(".", *deps)
     session.run("flake8", *args)
 
 
-@nox.session(python=python_versions)
+@session(python=python_versions)
 def mypy(session: Session) -> None:
     """Check types with mypy."""
     args = session.posargs
-    install_constrained_version(
-        session, "mypy", "types-docutils", "pytest", "sphinx", "nox"
-    )
+    deps = ["mypy", "types-docutils", "pytest", "sphinx", "nox", "nox-poetry"]
+    session.install(".", *deps)
     session.run("mypy", *args)
 
 
-@nox.session(python=python_versions)
+@session(python=python_versions)
 def typeguard(session: Session) -> None:
     """Check types at runtime with typeguard."""
     package = "sphinxawesome.sampdirective"
-    session.run("poetry", "install", "--no-dev", external=True)
-    install_constrained_version(session, "pytest", "typeguard")
+    deps = ["pytest", "typeguard"]
+    session.install(".", *deps)
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
 
 
-@nox.session(python="3.9")
+@session(python="3.9")
 def safety(session: Session) -> None:
     """Check for insecure dependencies with safety."""
     with tempfile.NamedTemporaryFile() as requirements:
@@ -86,29 +68,29 @@ def safety(session: Session) -> None:
             f"--output={requirements.name}",
             external=True,
         )
-        install_constrained_version(session, "safety")
+        session.install("safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
-@nox.session(python=python_versions[-1])
+@session(python=python_versions[-1])
 def coverage(session: Session) -> None:
     """Upload coverage report."""
-    install_constrained_version(session, "coverage[toml]", "codecov")
+    session.install(".", "coverage[toml]", "codecov")
     session.run("coverage", "xml", "--fail-under=0")
     session.run("codecov", *session.posargs)
 
 
-@nox.session(python=python_versions[-1])
+@session(python=python_versions[-1])
 def black(session: Session) -> None:
     """Format code with Black."""
     args = session.posargs or locations
-    install_constrained_version(session, "black")
+    session.install(".", "black")
     session.run("black", *args)
 
 
-@nox.session(python=python_versions[-1])
+@session(python=python_versions[-1])
 def isort(session: Session) -> None:
     """Reorder imports with isort."""
     args = session.posargs or locations
-    install_constrained_version(session, "isort")
+    session.install(".", "isort")
     session.run("isort", *args)
